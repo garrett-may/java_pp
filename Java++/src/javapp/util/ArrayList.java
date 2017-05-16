d37da93653ebb0d5ac6ddf8b51e333da4052302f
garrett-may <garrett.ls.may@gmail.com>
2017-05-16 13:24:16 +0100

    Added ArrayList class

---
+++ Java++/src/javapp/util/ArrayList.java
@@ -0,0 +1,16 @@
+package javapp.util;
+
+public class ArrayList<T> extends java.util.ArrayList<T> implements List<T>, Collection<T> {
+
+	private static final long serialVersionUID = 7358471882735023697L;
+
+	@SuppressWarnings("unchecked")
+	public ArrayList<T> copy(){
+		ArrayList<T> copy = new ArrayList<T>();
+		for(T t : this) {
+			copy.add(t);
+		}
+		return copy;
+	}
+	
+}
