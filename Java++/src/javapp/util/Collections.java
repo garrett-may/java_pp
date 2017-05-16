fdd5c61c074974d4c403846e0ffbf49d08b1b484
garrett-may <garrett.ls.may@gmail.com>
2017-05-16 13:29:39 +0100

    Added Collections library class

---
+++ Java++/src/javapp/util/Collections.java
@@ -0,0 +1,53 @@
+package javapp.util;
+
+import java.lang.reflect.Array;
+
+public final class Collections {
+	
+	private Collections() {
+		
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static <T> ArrayList<T> asArrayList(T...ts) {
+		ArrayList<T> list = new ArrayList<T>();
+		for(T t : ts) list.add(t);
+		return list;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static <T> LinkedList<T> asLinkedList(T...ts) {
+		LinkedList<T> list = new LinkedList<T>();
+		for(T t : ts) list.add(t);
+		return list;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static <T> Queue<T> asQueue(T...ts) {
+		return asLinkedList(ts);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static <T> Stack<T> asStack(T...ts) {
+		Stack<T> stack = new Stack<T>();
+		for(T t : ts) stack.push(t);
+		return stack;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static <T> HashSet<T> asHashSet(T...ts) {
+		HashSet<T> set = new HashSet<T>();
+		for(T t : ts) set.add(t);
+		return set;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static <T> T[] asArray(Collection<T> collection) {
+		T[] array = (T[]) Array.newInstance(collection.iterator().next().getClass(), collection.size());
+		int index = 0;
+		for(T T : collection) {
+			array[index++] = T;
+		}
+		return array;
+	}
+}
